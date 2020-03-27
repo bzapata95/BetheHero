56e651b425bce03d8e535d7cf55412556affe8f9
bzapata95 <bmzc95@gmail.com>
2020-03-27 18:19:12 -0500

    Test of integrations and unit

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,15 +1,18 @@
 const crypto = require("crypto");
 const connection = require("../database/connection");
 
+const generateUniqueId = require("../utils/generateUniqueId");
+
 module.exports = {
   async index(req, res) {
     const ongs = await connection("ongs").select("*");
     return res.json(ongs);
   },
+
   async create(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
 
-    const id = crypto.randomBytes(4).toString("hex");
+    const id = generateUniqueId();
 
     await connection("ongs").insert({
       id,
